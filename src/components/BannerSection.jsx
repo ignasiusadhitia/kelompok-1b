94e05e19248c622bab5c8c71edcae0c8544b6308
Andrean Nurdiana <andreannrdn@gmail.com>
2024-11-12 22:49:37 +0700

    feat(about): add comment on about pages

--- src/components/BannerSection.jsx
+++ src/components/BannerSection.jsx
@@ -4,10 +4,10 @@ import Typography from "../components/Typography";
 export default function BannerSection() {
   return (
       <div className="bg-dark-blue-100 flex flex-col align-middle py-10">
-      <Typography variant="strong" color="text-peach-red-100" className="flex text-4xl justify-center mb-3">
+      <Typography variant="strong" color="text-peach-red-100" className="flex text-4xl justify-center mb-1">
         About
       </Typography>
-      <Typography variant="pretext" color="text-off-white" className="flex justify-center text-sm mt-4">
+      <Typography variant="pretext" color="text-off-white" className="flex justify-center text-sm mt-2">
         Home / About
       </Typography>
     </div>
