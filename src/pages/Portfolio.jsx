83a94178f82f569b263957800195ec024f9760f7
ignasiusadhitia <ignasius.yuda.adhitia@gmail.com>
2024-11-13 13:29:15 +0700

    fix: resolve conflict

--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -1,20 +1,12 @@
 import React from "react";
 import OurWorkSection from "../components/OurWorkSection";
 import BannerSection from "../components/BannerSection";
-<<<<<<< HEAD
 import QuoteCard from "../components/QuoteCard";
 
 const Portfolio = () => {
   return (
     <div>
       <BannerSection title={"Portfolio"} breadCrumb={"Home / Portfolio"} />
-=======
-const Portfolio = () => {
-  return (
-    <div>
-      {/* send props for change title and bread crumb */}
-      <BannerSection title="Portofolio" breadCrumb="Home / Portofolio"  />
->>>>>>> 65c88dea33b0e3ec46a4257452bee9f5e2a5d77a
       <OurWorkSection />
       <QuoteCard />
     </div>
